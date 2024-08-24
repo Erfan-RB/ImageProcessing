820f73b1a1f12176fe665f2621b98dca8604c47c
Erfan <103189024+Erfan-RB@users.noreply.github.com>
2024-08-24 16:57:30 +0330

    Create 08-Corner Detection.py

---
+++ 08-Corner Detection.py
@@ -0,0 +1,12 @@
+#Corner Detection
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+# Read in the image
+image = cv.imread('.png')
+image_gr = cv.imread('.png', 0)
+corners = cv.cornerHarris(image_gr, 3, 10, 0.04)
+corners_dilated = cv.dilate(corners, None)
+image[corners_dilated > 0.01*corners_dilated.max()] = [255,0,0]
+plt.imshow(image)
+plt.imshow()

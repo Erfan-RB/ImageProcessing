ed0239809860975be985c2a36d3f16ff30184665
Erfan <103189024+Erfan-RB@users.noreply.github.com>
2024-08-25 19:46:13 +0330

    Create 09-Template Matching.py

---
+++ 09-Template Matching.py
@@ -0,0 +1,15 @@
+#Template Matching
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+image = cv.imread("", 0)
+template = cv.imread("g", 0)
+w, h = template.shape
+result = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
+threshold = 0.7
+locations = np.where(result >= threshold)
+for point in zip(*locations[::-1]):
+    cv.rectangle(image, point, (point[0]+ h, point[1]+w), (255,255,0), 2)
+
+plt.imshow(image)
+plt.show()

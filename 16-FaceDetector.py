92e6125e7795ebce823df140e278a5111e0df113
Erfan <103189024+Erfan-RB@users.noreply.github.com>
2024-09-01 19:23:24 +0330

    Create 16-FaceDetector.py

---
+++ 16-FaceDetector.py
@@ -0,0 +1,21 @@
+#FaceDetector
+import cv2 as cv
+import numpy as np 
+from cvzone.FaceDetectionModule import FaceDetector
+from cvzone.FaceMeshModule import FaceMeshDetector
+
+cap =cv.VideoCapture(1)
+detector = FaceDetector()
+meshdetector = FaceMeshDetector(maxFaces=1)
+while(True):
+    rec, frame = cap.read()
+    frame, bbox = detector.findFaces(frame)
+    frame, faces = meshdetector.findFaceMesh(frame)
+    if bbox:
+        center = bbox[0]["center"]
+        cv.imshow('frame', frame)
+    keyexit = cv.waitKey(5) & 0xFF
+    if keyexit == 27:
+        break
+cv.destroyAllWindows()
+cap.release()
